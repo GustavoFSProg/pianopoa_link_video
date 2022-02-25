3191d607df1cdb49c5effce72b0936ab820ca140
Rodrigo Avellar <roavellarm@gmail.com>
2022-02-24 21:44:46 -0300

    backup

--- ConfigUpload.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-
- $config_upload["renomeia"]=false;
- $config_upload["tamanho"]=2200000000000000;
- $config_upload["caminho_absoluto"]="/home/pianopoa/public_html/upload/"; 
-$config_upload["caminho_absoluto_2"]="/home/pianopoa/upload_2/"; 
-
-
-?>
